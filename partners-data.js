30b126bf31a7d694178ca2fb964f1204c3b9823f
master-stm <twisterleader@yahoo.com>
2025-06-01 18:36:55 +0300

    commit 6.1.2025
    added partners slider and commented sponsors

---
+++ partners-data.js
@@ -0,0 +1,56 @@
+export const partnersData = [
+    {
+        name: "Quadron"
+    },
+    {
+        name: "KARS (KU)"
+    },
+    {
+        name: "CPES"
+    },
+    {
+        name: "KITS"
+    },
+    {
+        name: "NCSC"
+    },
+    {
+        name: "Huawei"
+    },
+    {
+        name: "Palo Alto Networks"
+    },
+    {
+        name: "West Hosting"
+    },
+    {
+        name: "Tech Club GUST"
+    },
+    {
+        name: "PAAET"
+    },
+    {
+        name: "Agility"
+    },
+    {
+        name: "Kuwait Innovation Center"
+    },
+    {
+        name: "KOC"
+    },
+    {
+        name: "Cyber Talents"
+    },
+    {
+        name: "Hemma.Tech"
+    },
+    {
+        name: "Kuwait AI"
+    },
+    {
+        name: "KFAS"
+    },
+    {
+        name: "CODED"
+    }
+];
